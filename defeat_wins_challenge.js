b57852a690b377b8710f219a22c02a35a7cf2c8d
LizulDev <luizfelipekauedasilva@gmail.com>
2024-01-10 17:20:46 -0300

    docs: Update on project 'defeat_wins_challenge.js'

--- defeat_wins_challenge.js
+++ defeat_wins_challenge.js
@@ -2,67 +2,49 @@
 //Made by Luiz Felipe
 //07/01/2024
 
+function rankedCalculator(victory, defeat){
+    return victory - defeat;
+}
 
-let xpQuantities = [[25, 15], [68,46], [86, 70], [21, 45]] 
-let nivel;
-let counter = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*for (counter = 0; counter < 9; counter++){
-    if (xpQuantities[counter] < 1000){
-        nivel = "Ferro"
+function heroLevel(victoryScore){
+    let level;
+    if (victoryScore <= 10) {
+        level = "Ferro";
+    } else if (victoryScore <= 20) {
+        level = "Bronze";
+    } else if (victoryScore <= 50) {
+        level = "Prata";
+    } else if (victoryScore <= 80) {
+        level = "Ouro";
+    } else if (victoryScore <= 90) {
+        level = "Diamante";
+    } else if (victoryScore <= 100) {
+        level = "Lendário";
+    } else {
+        level = "Imortal";
     }
-    else if(xpQuantities[counter] > 1000 && xpQuantities[counter] <= 2000 ){
-        nivel = "Bronze"
+    console.log("============================================================");
+    if (victoryScore < 0) {
+        absoluteNumber = Math.abs(victoryScore);
+        console.log(`O herói tem o saldo negativo de ${absoluteNumber} e está no nível de ${level}`);
+    } else {
+        console.log(`O herói tem o saldo de ${victoryScore} e está no nível de ${level}`);
+        console.log("\n");
     }
-    else if(xpQuantities[counter] > 2000 && xpQuantities[counter] <= 5000 ){
-        nivel = "Prata"
-    }
-    else if(xpQuantities[counter] > 5000 && xpQuantities[counter] <= 7000 ){
-        nivel = "Ouro"
-    }
-    else if(xpQuantities[counter] > 7000 && xpQuantities[counter] <= 8000 ){
-        nivel = "Platina"
-    }
-    else if(xpQuantities[counter] > 8000 && xpQuantities[counter]<= 9000 ){
-        nivel = "Ascendente"
-    }
-    else if(xpQuantities[counter] > 9000 && xpQuantities[counter] <= 10000 ){
-        nivel = "Imortal"
-    }
-    else if(xpQuantities[counter] > 10000 && xpQuantities[counter] <= 1000000){
-        nivel = "Radiante"
-    }
-    else if (xpQuantities[counter] > 1000000){
-        nivel = "Grão Mestre Felipão"
-    }
-    console.log("============================================================")
-    console.log("O herói tem o saldo de " + saldoDeVitorias + " está no nivel de " + nivel)
+   
     
-}*/
+}
+function main(){
+    let victoriesAndDefeats = [[25, 15], [68,46], [86, 70], [21, 45]]; 
+    let victoryScore;
 
-console.log("\n")
+    for (let counter = 0; counter < victoriesAndDefeats.length; counter++) {
+        victoryScore = rankedCalculator(victoriesAndDefeats[counter][0], victoriesAndDefeats[counter][1]);
+        heroLevel(victoryScore);
+    }
+
+}
+
+main();
+
 
