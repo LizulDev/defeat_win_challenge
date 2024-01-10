2935808b0e1e7a280d655e6404e241b61c35a331
LizulDev <luizfelipekauedasilva@gmail.com>
2024-01-10 16:04:50 -0300

    Inserting main project and modifying Readme

---
+++ defeat_wins_challenge.js
@@ -0,0 +1,68 @@
+// Desafio pela DIO para cálculo de partidas rankeadas utilizando funções;
+//Made by Luiz Felipe
+//07/01/2024
+
+
+let xpQuantities = [[25, 15], [68,46], [86, 70], [21, 45]] 
+let nivel;
+let counter = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for (counter = 0; counter < 9; counter++){
+    if (xpQuantities[counter] < 1000){
+        nivel = "Ferro"
+    }
+    else if(xpQuantities[counter] > 1000 && xpQuantities[counter] <= 2000 ){
+        nivel = "Bronze"
+    }
+    else if(xpQuantities[counter] > 2000 && xpQuantities[counter] <= 5000 ){
+        nivel = "Prata"
+    }
+    else if(xpQuantities[counter] > 5000 && xpQuantities[counter] <= 7000 ){
+        nivel = "Ouro"
+    }
+    else if(xpQuantities[counter] > 7000 && xpQuantities[counter] <= 8000 ){
+        nivel = "Platina"
+    }
+    else if(xpQuantities[counter] > 8000 && xpQuantities[counter]<= 9000 ){
+        nivel = "Ascendente"
+    }
+    else if(xpQuantities[counter] > 9000 && xpQuantities[counter] <= 10000 ){
+        nivel = "Imortal"
+    }
+    else if(xpQuantities[counter] > 10000 && xpQuantities[counter] <= 1000000){
+        nivel = "Radiante"
+    }
+    else if (xpQuantities[counter] > 1000000){
+        nivel = "Grão Mestre Felipão"
+    }
+    console.log("============================================================")
+    console.log("O herói tem o saldo de " + saldoDeVitorias + " está no nivel de " + nivel)
+    
+}*/
+
+console.log("\n")
+
